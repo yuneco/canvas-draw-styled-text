e174db908e992acd7e09886d3f1f7f194aa95819
yuneco <yukiyuneco@gmail.com>
2023-09-11 00:10:24 +0900

    add docs for pages

--- sample/vite.config.sample.ts
+++ sample/vite.config.sample.ts
@@ -1,7 +1,10 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-
 export default defineConfig({
   root: resolve(__dirname),
-  })
+  base: ".",
+  build: {
+    outDir: "../docs"
+  }
+})

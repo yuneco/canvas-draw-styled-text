81068ad6a924e3445ebce1ab67516254c52afa3e
yuneco <yukiyuneco@gmail.com>
2024-03-11 16:16:41 +0900

    fix incorrect word char length for word that char has multi codepoints

--- src/drawText/breakLine.ts
+++ src/drawText/breakLine.ts
@@ -1,40 +1,51 @@
-import { CharMetrix, LineMetrix } from "./defs/metrix"
-import {LineBreaker} from 'css-line-break';
+import { CharMetrix, LineMetrix } from './defs/metrix'
+import { LineBreaker } from 'css-line-break'
 
 type Word = {
   /** char count */
-  length: number;
+  length: number
   /** width(px) of word */
-  width: number;
+  width: number
   /** char metrixes in this word */
-  chars: CharMetrix[];
+  chars: CharMetrix[]
 }
 
 const createBreaker = (text: string) => {
   return LineBreaker(text, {
     lineBreak: 'strict',
-    wordBreak: 'normal'
-});
+    wordBreak: 'normal',
+  })
 }
 type Breaker = ReturnType<typeof createBreaker>
 
 /**
  * get next word from text
  * @param breaker line breaker for source text
- * @param CharMetrixes metrix of all char
+ * @param charMetrixes rest chars. NOTE: this function will consume this array.
  * @returns next word. if no word, return undefined
  */
-const nextWord = (breaker: Breaker, CharMetrixes: CharMetrix[]): Word | undefined => {
+const nextWord = (breaker: Breaker, charMetrixes: CharMetrix[]): Word | undefined => {
   const lb = breaker.next()
   if (lb.done) {
     return undefined
   }
   const start = lb.value.start
   const end = lb.value.end
-  const length = end - start
-  const chars = CharMetrixes.slice(start, end)
+  let restCodePointCount = end - start
+  const chars: CharMetrix[] = []
+  while (restCodePointCount > 0) {
+    const char = charMetrixes.shift()
+    if (!char) {
+      break
+    }
+    chars.push(char)
+    // reduce code point count.
+    // char.textChar is a single glyph, but it may be a surrogate pair.
+    restCodePointCount -= [...char.textChar].length
+  }
+
   const width = chars.reduce((sum, c) => sum + c.metrix.width, 0)
-  return { length, width, chars }
+  return { length: chars.length, width, chars }
 }
 
 /**
@@ -46,6 +57,7 @@ const nextWord = (breaker: Breaker, CharMetrixes: CharMetrix[]): Word | undefine
  */
 export const lineBreakWithCharMetrixes = (text: string, charMetrixes: CharMetrix[], maxWidth: number): LineMetrix[] => {
   const breaker = createBreaker(text)
+  const restChars = [...charMetrixes]
   const lines: LineMetrix[] = []
   const newLine = () => {
     const l = { at: 0, width: 0, lineAscent: 0, lineDescent: 0, lineMargin: 0 }
@@ -54,9 +66,9 @@ export const lineBreakWithCharMetrixes = (text: string, charMetrixes: CharMetrix
   }
   let line: LineMetrix = newLine()
 
-  let index = 0;
-  while (index < charMetrixes.length) {
-    const word = nextWord(breaker, charMetrixes);
+  let index = 0
+  while (restChars.length > 0) {
+    const word = nextWord(breaker, restChars)
     if (!word) {
       break
     }
@@ -67,10 +79,10 @@ export const lineBreakWithCharMetrixes = (text: string, charMetrixes: CharMetrix
     }
 
     line.width += word.width
-    line.lineAscent = Math.max(line.lineAscent, ...word.chars.map(c => c.metrix.fontBoundingBoxAscent) )
-    line.lineDescent = Math.max(line.lineDescent, ...word.chars.map(c => c.metrix.fontBoundingBoxDescent))
+    line.lineAscent = Math.max(line.lineAscent, ...word.chars.map((c) => c.metrix.fontBoundingBoxAscent))
+    line.lineDescent = Math.max(line.lineDescent, ...word.chars.map((c) => c.metrix.fontBoundingBoxDescent))
 
-    index += word.length;
+    index += word.length
 
     const isEndOfLine = word.chars.at(-1)?.textChar === '\n'
     if (isEndOfLine) {
@@ -79,5 +91,4 @@ export const lineBreakWithCharMetrixes = (text: string, charMetrixes: CharMetrix
     }
   }
   return lines
-
 }
